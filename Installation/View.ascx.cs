993d87fc240d9b102c8ec741321c84b4a8e6d4c4
bitboxx <info@bitboxx.net>
2016-01-29 00:17:51 +0100

    Edited Readme

--- Installation/View.ascx.cs
+++ Installation/View.ascx.cs
@@ -3,7 +3,7 @@
 namespace Bitboxx.DNNModules.BBImageStory
 {
     [DNNtc.ModuleDependencies(DNNtc.ModuleDependency.CoreVersion, "08.00.00")]
-    [DNNtc.PackageProperties("BBImageStory_Module", 1, "BBImageStory Module", "An image story module", "BBAngular.png", "Torsten Weggen", "bitboxx solutions", "http://www.bitboxx.net", "info@bitboxx.net", true)]
+    [DNNtc.PackageProperties("BBImageStory_Module", 1, "BBImageStory Module", "DNN Module for creating image stories, step-by-step guides or image galleries with subtexts", "BBAngular.png", "Torsten Weggen", "bitboxx solutions", "http://www.bitboxx.net", "info@bitboxx.net", true)]
     [DNNtc.ModuleProperties("BBImageStory_Module", "BBImageStory Module", 0)]
     [DNNtc.ModuleControlProperties("", "BBImageStory", DNNtc.ControlType.View, "", false, false)]
     public partial class ViewDummy : PortalModuleBase
